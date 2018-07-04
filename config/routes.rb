ea8ccfddb505405831ad85cada36f7c7a415f65a
froover <dio22038@gmail.com>
2018-07-04 14:18:12 +0900

    letter_openerの記述削除

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,5 @@ Rails.application.routes.draw do
 
   get 'pages/show'
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
