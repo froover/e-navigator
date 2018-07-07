c82877cc92b3923f44d8673182d5a616a39daec3
froover <dio22038@gmail.com>
2018-07-07 17:26:03 +0900

    ユーザー一覧ページをpages/showからusers/indexに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,4 @@ class UsersController < ApplicationController
   def index
   end
 
-  def show
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     root to: "users/sessions#new"
   end
 
-  get 'pages/show'
+  get 'users/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
