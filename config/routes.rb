da22f4435d3281694707f94f26d68759dc6cae5c
froover <dio22038@gmail.com>
2018-07-09 19:19:27 +0900

    devise用コントローラーを使わない方法に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-      :registrations => 'users/registrations',
-      :sessions => 'users/sessions'
-  }
+  devise_for :users
 
-  devise_scope :user do
-    root to: "users/sessions#new"
-  end
-
+  root 'users#index'
   get 'users/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
