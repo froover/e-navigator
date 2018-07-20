b8e3c2564b0682fe2a5cda6140035553f5b426cc
froover <dio22038@gmail.com>
2018-07-20 21:36:17 +0900

    承認状況をenumで設定

--- db/migrate/20180710063510_create_interviews.rb
+++ db/migrate/20180710063510_create_interviews.rb
@@ -2,7 +2,7 @@ class CreateInterviews < ActiveRecord::Migration[5.1]
   def change
     create_table :interviews do |t|
       t.datetime :interview_date
-      t.string :approval, default: "保留"
+      t.integer :approval, default: 0
       t.string :user_id
       t.references :user, foreign_key: true, type: :integer
 
