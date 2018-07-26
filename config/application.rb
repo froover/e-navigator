c012eac01808eb6368afb486be40910805994a8a
froover <dio22038@gmail.com>
2018-07-26 22:55:08 +0900

    日付関連の修正

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module ENavigator
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
