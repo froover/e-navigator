979e91ade0b5a00d03437bea72c95783ba76fb4c
froover <dio22038@gmail.com>
2018-07-31 03:02:49 +0900

    生年月日を登録必須に

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :interviews, dependent: :destroy
+  validates :birthday, presence: true
   enum gender: { male: 0, female: 1 }
 
   # 生年月日から年齢計算
