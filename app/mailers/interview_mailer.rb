ba8f221ad061d346336f9b25b054990d4ba561c6
froover <dio22038@gmail.com>
2018-08-03 16:37:18 +0900

    申請時のメール送信

--- app/mailers/interview_mailer.rb
+++ app/mailers/interview_mailer.rb
@@ -1,5 +1,11 @@
 class InterviewMailer < ApplicationMailer
 
+  def send_when_application(user, current_user)
+    @user = user
+    @current_user = current_user
+    mail to: @current_user.email, subject: "面接希望日の申請がありました"
+  end
+
   def send_when_confirm(user, current_user, interview)
     @user = user
     @current_user = current_user
