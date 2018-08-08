fb1af4d50984fc7765cbebd2c8b8bd418c55f3d8
froover <dio22038@gmail.com>
2018-08-09 01:49:01 +0900

    面接日確定時のメールを双方に送るように修正

--- app/mailers/interview_mailer.rb
+++ app/mailers/interview_mailer.rb
@@ -10,6 +10,6 @@ class InterviewMailer < ApplicationMailer
     @user = user
     @current_user = current_user
     @interview = user.interviews.find_by(approval: "approved")&.interview_date&.to_s(:datetime)
-    mail to: @user.email, subject: "面接日が確定しました"
+    mail to: [@user.email, @current_user.email], subject: "面接日が確定しました"
   end
 end
